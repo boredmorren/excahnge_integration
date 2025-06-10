e9ec28cf643dd7d9728fac3072c9a306b05ed4ce
boredmorren <chilleam.biz@gmail.com>
2025-06-10 17:23:54 +0300

    create main api.

---
+++ exchange_api.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify 
+import os, requests
+
+APIKEY = os.environ.get('APIKEY')
+app = Flask(__name__)
+path = 'https://v6.exchangerate-api.com/v6/' + APIKEY
+
+
+@app.route('/')
+def index():
+    return '''/get_latest'''
+
+@app.route('/get_latest')
+def get_latest():
+    response = requests.get(path + '/latest/RUB').json()
+    return jsonify(response)
+
+
+def main():
+    app.run()
